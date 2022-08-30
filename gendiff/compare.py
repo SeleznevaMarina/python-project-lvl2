610a15d4c813f874feb85507762e80fc98a0bcdf
Selezneva Marina <petrukhina.marina2011@yandex.ru>
2022-08-30 09:34:25 +0300

    add compare, step 3

---
+++ gendiff/compare.py
@@ -0,0 +1,28 @@
+import json, copy
+
+
+def generate_diff(file1, file2):
+
+    file_1 = json.load(open(file1))
+    file_2 = json.load(open(file2))
+    compare_dict = {}
+    keys = list(file_1.keys())
+    keys.extend(list(file_2.keys()))
+    keys = sorted(set(keys))
+    diff = '{\n'
+
+    for key in keys:
+
+        if key in file_1 and key in file_2 and file_1[key] == file_2[key]:
+            diff += f'    {key}: {file_1[key]}\n'
+        elif key in file_1:
+            diff += f'  + {key}: {file_1[key]}\n'
+
+            if key in file_2:
+                diff += f'  - {key}: {file_2[key]}\n'
+
+        elif key in file_2:
+            diff += f'  - {key}: {file_2[key]}\n'
+
+    diff += '}'
+    return diff
