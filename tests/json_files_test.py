b94109fbaf1e9202a31951d1909c0d44a73efa69
Selezneva Marina <petrukhina.marina2011@yandex.ru>
2022-10-14 16:46:38 +0300

    linter

--- tests/json_files_test.py
+++ tests/json_files_test.py
@@ -6,6 +6,7 @@ from gendiff import generate_diff
 PATH1 = Path().absolute() / "tests" / "fixtures" / "file1.json"
 PATH2 = Path().absolute() / "tests" / "fixtures" / "file2.json"
 
+
 @pytest.mark.parametrize("formarter, result_file", [("plain", "test_result.plain"), ("stylish", "test_result"), ("json", "test_result.json")])
 def test_generate_diff(formarter, result_file):
 
--- tests/yaml_files_test.py
+++ tests/yaml_files_test.py
@@ -6,6 +6,7 @@ from gendiff import generate_diff
 PATH1 = Path().absolute() / "tests" / "fixtures" / "file1.yml"
 PATH2 = Path().absolute() / "tests" / "fixtures" / "file2.yaml"
 
+
 @pytest.mark.parametrize("formarter, result_file", [("plain", "test_result.plain"), ("stylish", "test_result"), ("json", "test_result.json")])
 def test_generate_diff(formarter, result_file):
 
